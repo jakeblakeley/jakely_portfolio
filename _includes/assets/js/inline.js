324fc06b20a7ceb7382f08049f14c32e1ca72949
Jake Blakeley <jakeblakeley93@gmail.com>
2023-07-12 22:47:28 -0700

    core updates

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -168,6 +168,38 @@ document.addEventListener('mousemove', (event) => {
   rotateXOffset = mousePercentHeightRemapped * 0.2;
 });
 
+//gyro position
+document.addEventListener("click", (event) => {
+  onClick();
+});
+//get access for ios
+function onClick() {
+  if (typeof DeviceMotionEvent.requestPermission === 'function') {
+    // Handle iOS 13+ devices.
+    DeviceMotionEvent.requestPermission()
+      .then((state) => {
+        if (state === 'granted') {
+          window.addEventListener('devicemotion', handleOrientation);
+        } else {
+          console.error('Request to access the orientation was rejected');
+        }
+      })
+      .catch(console.error);
+  } else {
+    // Handle regular non iOS 13+ devices.
+    window.addEventListener('devicemotion', handleOrientation);
+  }
+}
+window.addEventListener('deviceorientation', handleOrientation);
+
+function handleOrientation(event) {
+  //const alpha = event.alpha;
+  const beta = event.beta;
+  const gamma = event.gamma;
+  // Do stuff...
+  console.log("b: " + beta + " g: " + gamma);
+}
+
 //animation loop
 function animate() {
   //render
